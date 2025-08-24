c2527f3556e65194c64575353ba473d2b4c73a52
elderlima7 <eldersantoss1981@gmail.com>
2025-08-24 02:50:19 -0300

    Game repository, service, controller

---
+++ src/main/java/com/elder/dslist/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.elder.dslist.repositories;
+
+import com.elder.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
